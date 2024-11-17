28fab07e89edd06b2c4bf7264ac01b4cf5b735d0
Erick El nino <erick.tshimbombo@yahoo.com>
2024-11-17 13:57:41 +0200

    updated small fixes ticket #001

--- middlewares/common/error.js
+++ middlewares/common/error.js
@@ -2,14 +2,29 @@
 const createError = require("http-errors");
 
 const notFoundHandler = (request, response, next) => {
-  next(createError(404, "your requested content was not found"));
+  const error = new Error(createError(404, "Page was not found"));
+  next(error);
 };
 
 const errorhandler = (error, request, response, next) => {
-  response.render("error", {
-    title: "Error page",
-    content: `${error.message}`,
-  });
+  response.locals.error =
+    process.env.NODE_ENV === "DEV" ? error : { message: error.message };
+
+  response.status(error.status || 500);
+  if (response.locals.html) {
+    // render html repsonse
+    response.render("error", {
+      title: "This is html error page",
+      content: `${error.message}`,
+    });
+  } else {
+    // JSON response
+
+    response.json({
+      title: "Error Found",
+      error: response.locals.error,
+    });
+  }
   next();
 };
 
