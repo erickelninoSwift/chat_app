aac144e469d209fd74a6805eda765ae1eadf906a
Erick El nino <erick.tshimbombo@yahoo.com>
2024-11-17 16:04:21 +0200

    small fix

--- middlewares/common/pageTitle.js
+++ middlewares/common/pageTitle.js
@@ -1,7 +1,8 @@
 const handlePageTitle = (pagetitle) => {
   return async (request, response, next) => {
-    response.locals.html = true;
-    response.locals.title = `${pagetitle} - ${process.env.PAGE_TITLE}`;
+    response.locals.html =
+      true &&
+      (response.locals.title = `${pagetitle} - ${process.env.PAGE_TITLE}`);
     next();
   };
 };
