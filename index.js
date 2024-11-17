fca3c7121f1e01115bcb04d9894afe0efd23dd41
Erick El nino <erick.tshimbombo@yahoo.com>
2024-11-17 02:03:29 +0200

    updated

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View Engine
+
+app.set("view engine", "ejs");
+
+// set Statict foldder
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// ====
+
+// parse cookies
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// ======
+
+// routes setup
+
 app.get("/", (request, response) => {
   return response.send("hello world");
 });
+// =======
+
+// Error Handling
+
+// =========
 
 const handleMongoDBConnection = async () => {
   mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}`).then(() => {
